2c385fd75d92f7f9e3053a6a7842d7498ada774c
Zahid Hasan <hasanzuav@gmail.com>
2012-02-18 16:04:09 -0500

    Finished first draft of badges classes, minor edit to challenges

--- Badges.js
+++ Badges.js
@@ -4,5 +4,22 @@
 // Created by Zahid Hasan: 2/18/2012
 
 function Badges(){
-	
+	this.acquiredBadges = ("");
+}
+
+// List of all existing badges
+function BadgeList{
+	var BadgeList= ("GetStarted","Builder","HelloWorld","Updater","HunterGatherer");
+}
+
+//Function to add a rewarded badge to the acquired badges
+function addAcquiredBadges(BadgeName){
+	var badgeIndex = this.acquiredBadges.indexOf(BadgeName); //find badge index
+	if(badgeIndex =-1) this.acquiredBadges.push(BadgeName); //add if not found
+}
+
+//Function to remove an rewarded badge to the acquired badges
+function removeAcquiredBadges(BadgeName){
+	var badgeIndex = this.acquiredBadges.indexOf(BadgeName); //find badge index
+	if(badgeIndex !=-1) this.acquiredBadges.splice(badgeIndex,BadgeName); //remove if found
 }
--- Challenges.js
+++ Challenges.js
@@ -19,18 +19,15 @@ this.comparesCompleted =0;
 ChallengeList(){
 
 //List of +1 challenges to test all challenge types
-var GetStartedChallenge = {name: "Get started!", viewsRequired: 1, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 100, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
+var GetStartedChallenge = {name: "Get started!", viewsRequired: 1, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 100, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "GetStarted"};
 
-var HelloWorldChallenge = {name: "Hello World!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 1, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 500, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
+var BuilderChallenge = {name: "Build it!", viewsRequired: 0, oganizesRequired: 1, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 500, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "Builder"}};
 
+var HelloWorldChallenge = {name: "Hello World!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 1, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 1000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "HelloWorld"};
 
-var BuilderChallenge = {name: "Build it!", viewsRequired: 0, oganizesRequired: 1, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 1000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
+var UpdaterChallenge = {name: "What do you know!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 1, comparesCompleted: 0, levelReward: 0, XPReward: 5000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "Updater"};
 
-
-var UpdaterChallenge = {name: "What do you know!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 1, comparesCompleted: 0, levelReward: 0, XPReward: 5000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
-
-
-var CompareChallenge = {name: "Get started!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 1, XPReward: 15000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
+var HunterGathererChallenge = {name: "Get started!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 1, XPReward: 15000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "HunterGatherer"};
 
 }
 
