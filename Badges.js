2b1e6c1a210b6edaf653c49a2ad5c44e27c8c27f
Zahid Hasan <hasanzuav@gmail.com>
2012-02-18 15:12:58 -0500

    Added first round of challenges including rewards and added a new badges class

---
+++ Badges.js
@@ -0,0 +1,8 @@
+// Badges Class
+// Badges Class contains all the badges that exist as well as keeps track of what the user has won.
+// get your bling on
+// Created by Zahid Hasan: 2/18/2012
+
+function Badges(){
+	
+}
--- Challenges.js
+++ Challenges.js
@@ -4,26 +4,43 @@
 // Created by Zahid Hasan: 2/17/2012
 
 function Challenges(){
-this.viewCompleted = 0;
-this.organizeCompleted =0;
-this.greetCompleted = 0;
-this.updateCompleted = 0;
-this.compareCompleted =0;
+this.viewsCompleted = 0;
+this.organizesCompleted =0;
+this.greetsCompleted = 0;
+this.updatesCompleted = 0;
+this.comparesCompleted =0;
 }
 
 // ChallengeList organizing the tasks needed to complete a challenge, the rewards reeped for completing a challenge, and any flags that need to be triggered within the game.
 // challenges(view, organize, greet, update info, compare)
 // rewards (xps, skill, karma, credits, levels)
 // flag(badge list, lockouts, new skill sets)
+
 ChallengeList(){
 
-var cat = {colour: "grey", name: "Spot", size: 46};
-cat.size = 47;
-show(cat.size);
-delete cat.size;
+//List of +1 challenges to test all challenge types
+var GetStartedChallenge = {name: "Get started!", viewsRequired: 1, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 100, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
 
+var HelloWorldChallenge = {name: "Hello World!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 1, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 500, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
+
+
+var BuilderChallenge = {name: "Build it!", viewsRequired: 0, oganizesRequired: 1, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 0, levelReward: 0, XPReward: 1000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
+
+
+var UpdaterChallenge = {name: "What do you know!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 1, comparesCompleted: 0, levelReward: 0, XPReward: 5000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
+
+
+var CompareChallenge = {name: "Get started!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesCompleted: 1, XPReward: 15000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0};
 
 }
 
-Challenge
+//Clear all challenges in a given category
+function ClearCompletedChallenges(challengesCompleted){
 
+this.challengesCompleted = 0;
+this.challengesCompleted;
+}
+//Delta challeneges changes the completed challenges by delta
+function DeltaCompletedChallenges(challengesCompleted, delta){S
+this.challengesCompleted += delta;
+}
