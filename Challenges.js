b021ac8f92083e00286d81adcbfbf0f9c1b89af6
Zahid Hasan <hasanzuav@gmail.com>
2012-03-02 21:39:09 -0500

    added struc for challenges, pass off

--- Challenges.js
+++ Challenges.js
@@ -1,15 +1,36 @@
-// Challenges Class
+// challenges Class
 // Challenges contains all challenges, metrics to accomplish the task, as well as rewards given for completion.
-//yo mama is a challenge
 // Created by Zahid Hasan: 2/17/2012
 
 //initialization
-function Challenges(){
-this.viewsCompleted = 0;
-this.organizesCompleted =0;
-this.greetsCompleted = 0;
-this.updatesCompleted = 0;
-this.comparesCompleted =0;
+function challenges()
+{
+	this.viewsCompleted = 0;
+	this.organizesCompleted =0;
+	this.greetsCompleted = 0;
+	this.updatesCompleted = 0;
+	this.comparesCompleted =0;
+	this.challengeList = [];
+	this.challengeList.push(new challengeStructure("Get started!", 1, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0));
+}
+
+function challengeStructure(name, viewsRequired, organizesRequired, greetsRequired, updatesRequired, comparesRequired, levelReward, XPReward, creditsReward, skillPointsReward, karmaPointsReward, badgeFlag)
+{
+	//challenge name
+	this.name= name;
+	//challenge demands
+	this.viewsRequired= viewsRequired;
+	this.organizesRequired= organizesRequired;
+	this.greetsRequired= greetsRequired;
+	this.updateRequired= updatesRequired;
+	this.comparesRequired= comparesRequired;
+	//challenge rewards
+	this.levelReward= levelReward; 
+	this.XPReward= XPReward;
+	this.creditsReward= creditsReward;
+	this.skillPointsReward= skillPointsReward;
+	this.karmaPointsReward= karmaPointsReward;
+	this.badgeFlag= badgeFlag;
 }
 // ChallengeList organizing the tasks needed to complete a challenge, the rewards reeped for completing a challenge, and any flags that need to be triggered within the game.
 // challenges(view, organize, greet, update info, compare)
@@ -17,34 +38,40 @@ this.comparesCompleted =0;
 // flag(badge list, lockouts, new skill sets)
 
 // Temporary initalizing challenge 
-Challenges.prototype.ChallengeList = function(){
+function ChallengesList(){
 //Upload to Server// will be on server
 
 //List of +1 challenges to test all challenge types
-var GetStartedChallenge = {name: "Get started!", viewsRequired: 1, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesRequired: 0, levelReward: 0, XPReward: 100, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "GetStarted"};
+var GetStartedChallenge = {name: "Get started!", viewsRequired: 1, organizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesRequired: 0, levelReward: 0, XPReward: 100, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeFlag: 0};
 
-var BuilderChallenge = {name: "Build it!", viewsRequired: 0, oganizesRequired: 1, greetsRequired: 0, updatesRequired: 0, comparesRequired: 0, levelReward: 0, XPReward: 500, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "Builder"};
+var BuilderChallenge = {name: "Build it!", viewsRequired: 0, organizesRequired: 1, greetsRequired: 0, updatesRequired: 0, comparesRequired: 0, levelReward: 0, XPReward: 500, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeFlag: 0};
 
-var HelloWorldChallenge = {name: "Hello World!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 1, updatesRequired: 0, comparesRequired: 0, levelReward: 0, XPReward: 1000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "HelloWorld"};
+var HelloWorldChallenge = {name: "Hello World!", viewsRequired: 0, organizesRequired: 0, greetsRequired: 1, updatesRequired: 0, comparesRequired: 0, levelReward: 0, XPReward: 1000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeFlag: 0};
 
-var UpdaterChallenge = {name: "What do you know!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 1, comparesRequired: 0, levelReward: 0, XPReward: 5000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "Updater"};
+var UpdaterChallenge = {name: "What do you know!", viewsRequired: 0, organizesRequired: 0, greetsRequired: 0, updatesRequired: 1, comparesRequired: 0, levelReward: 0, XPReward: 5000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeFlag: 0};
 
-var HunterGathererChallenge = {name: "Hunter Gatherer!", viewsRequired: 0, oganizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesRequired: 1, XPReward: 15000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeReward: "HunterGatherer"};
+var HunterGathererChallenge = {name: "Hunter Gatherer!", viewsRequired: 0, organizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesRequired: 1, XPReward: 15000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeFlag: 0};
 }
 
 //Clear all challenges in a given category
 //Server Database for keeeping traking of challenge that are already completed
 //c
-Challenges.prototype.ClearCompletedChallenges = function(challengesCompleted){
+challenges.prototype.ClearCompletedChallenges = function(challengesCompleted){
 // TODO : When server works : update the user challenge completed database with erhte challenge.
 }
 
 //Delta challeneges changes the completed challenges by delta
-Challenges.prototype.DeltaCompletedChallenges = function(challengesCompleted, delta){
+challenges.prototype.DeltaCompletedChallenges = function(challengesCompleted, delta){
 this.challengesCompleted += delta;
 }
 
 //import from server
-Challenges.prototype.importData = function(json){
+challenges.prototype.importData = function(json){
 	//TODO : Server data import
+}
+//A test for adding listed elements in the miniChallenges box
+challenges.prototype.generateText = function()
+{
+	var x = this.challengeList[0].name;
+		document.getElementById('challengeList').innerHTML = x;
 }
