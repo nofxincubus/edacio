ba4b7507b70afbf4b7fd0ce3065aa8084b2db3ff
Zahid Hasan <hasanzuav@gmail.com>
2012-03-04 21:51:15 -0500

    fixed challenge list to generate visual from predefined list of challenges.

--- Challenges.js
+++ Challenges.js
@@ -2,7 +2,16 @@
 // Challenges contains all challenges, metrics to accomplish the task, as well as rewards given for completion.
 // Created by Zahid Hasan: 2/17/2012
 
-//initialization
+//initialization of styles
+function setupChallengeStyle()
+{
+	var x = document.getElementById('miniChallenges');
+	x.setAttribute('style',"float:right;width:300px;height:420px;background-color:lightgrey;-moz-border-radius: 15px;border-radius: 15px;");
+	//document.body.setAttribute('style',"background-color:SkyBlue;");
+	//document.getElementsByClassName(.setAttribute('style',"background:red;width:;");
+
+}
+//initialization of variables
 function challenges()
 {
 	this.viewsCompleted = 0;
@@ -12,6 +21,26 @@ function challenges()
 	this.comparesCompleted =0;
 	this.challengeList = [];
 	this.challengeList.push(new challengeStructure("Get started!", 1, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0));
+	this.challengeList.push(new challengeStructure("Build it!", 0, 1, 0, 0, 0, 0, 500, 0, 0, 0, 0));
+	this.challengeList.push(new challengeStructure("Hello World!", 0, 0, 1, 0, 0, 0, 5000, 0, 0, 0, 0));
+	this.challengeList.push(new challengeStructure("Hunter!", 0, 0, 0, 1, 0, 0, 15000, 0, 0, 0, 0));
+}
+// Create minichallenges list html and css 
+challenges.prototype.createChallenges =function(challengeStruct)
+{
+	for(var challengeCount = 0; challengeCount<5; challengeCount++)
+	{
+		var challengeListItem = document.createElement('div');
+		challengeListItem.setAttribute('id', "challengeListItem");
+		challengeListItem.setAttribute('style', "width:300px;height:60px;");
+		challengeListItem.innerHTML='<img style="position:relative;top:3px;" src="awardsNo.png"/><h4><b  style="position:relative;top:-60px;left:50px;">'+challengeStruct[challengeCount].name+'</b></h4>'
+			+'<h5><font id="view"  style="position:relative;top:-105px;left:150px;text-align:right;">View:</font><font  id="organize" style="position:relative;top:-105px;left:180px;text-align:right;">Organize:</font></h5>'
+			+'<h5><font id="greet" style="position:relative;top:-115px;left:145px;text-align:right;">Greet:</font><font id="update" style="position:relative;top:-115px;left:190px;">Update:</font></h5>'
+			+'<h4><font id="viewValue"  style="position:relative;top:-183px;left:195px;text-align:right;">'+challengeStruct[challengeCount].viewsRequired+'</font><font id="organizeValue"  style="position:relative;top:-183px;left:275px;text-align:right;">'+challengeStruct[challengeCount].organizesRequired+'</font></h4>'
+			+'<h4><font id="greetValue"  style="position:relative;top:-196px;left:195px;text-align:right;">'+challengeStruct[challengeCount].greetsRequired+'</font><font id="updateValue"  style="position:relative;top:-196px;left:275px;text-align:right;">'+challengeStruct[challengeCount].updatesRequired+'</font></h4>';
+
+		document.getElementById("challengeList").appendChild(challengeListItem);
+	}
 }
 
 function challengeStructure(name, viewsRequired, organizesRequired, greetsRequired, updatesRequired, comparesRequired, levelReward, XPReward, creditsReward, skillPointsReward, karmaPointsReward, badgeFlag)
@@ -22,7 +51,7 @@ function challengeStructure(name, viewsRequired, organizesRequired, greetsRequir
 	this.viewsRequired= viewsRequired;
 	this.organizesRequired= organizesRequired;
 	this.greetsRequired= greetsRequired;
-	this.updateRequired= updatesRequired;
+	this.updatesRequired= updatesRequired;
 	this.comparesRequired= comparesRequired;
 	//challenge rewards
 	this.levelReward= levelReward; 
@@ -37,6 +66,7 @@ function challengeStructure(name, viewsRequired, organizesRequired, greetsRequir
 // rewards (xps, skill, karma, credits, levels)
 // flag(badge list, lockouts, new skill sets)
 
+/* this shit is dead
 // Temporary initalizing challenge 
 function ChallengesList(){
 //Upload to Server// will be on server
@@ -52,7 +82,7 @@ var UpdaterChallenge = {name: "What do you know!", viewsRequired: 0, organizesRe
 
 var HunterGathererChallenge = {name: "Hunter Gatherer!", viewsRequired: 0, organizesRequired: 0, greetsRequired: 0, updatesRequired: 0, comparesRequired: 1, XPReward: 15000, creditsReward: 0, skillPointsReward: 0, karmaPointsReward: 0, badgeFlag: 0};
 }
-
+*/
 //Clear all challenges in a given category
 //Server Database for keeeping traking of challenge that are already completed
 //c
@@ -75,3 +105,4 @@ challenges.prototype.generateText = function()
 	var x = this.challengeList[0].name;
 		document.getElementById('challengeList').innerHTML = x;
 }
+
